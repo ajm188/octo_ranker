4cbf39e4341cddb93c6884aa9fd02709084b1d27
Andrew Mason <mason@case.edu>
2015-04-10 00:07:42 -0400

    add comments

--- lib/octo_ranker.rb
+++ lib/octo_ranker.rb
@@ -5,6 +5,15 @@ require 'octokit'
 
 module OctoRanker
   class << self
+    # Rank the members of +organization+ according to their scores.
+    # The score for a user on a repo is computed by some function of
+    # the number of commits the user has on the repo and the number of
+    # stars the repo has (contributions to more popular repos should be
+    # significant). See OctoRanker::User#add_repo for how this function
+    # is defined. The user's total score is the sum of the user's score
+    # on each repo owned by +organization+.
+    #
+    # Returns an array of OctoRanker::User objects.
     def rank_members(organization)
       users = Hash.new { |h, k| h[k] = OctoRanker::User.new }
       Octokit.organization_repositories(organization).each do |repo|
--- lib/octo_ranker/user.rb
+++ lib/octo_ranker/user.rb
@@ -7,6 +7,10 @@ module OctoRanker
       @login, @score = login, 0
     end
 
+    # Add the score for a repo to the score for this user.
+    # Currently, the score for a repo is defined as the product
+    # of the number of commits the user has on the repo and the
+    # number of stars on the repo.
     def add_repo(commits, stars)
       @score += commits * stars
     end
